58855f8bd870eeeb45d986d06883e0764715cc94
José Fernando <fernandoscsul@gmail.com>
2020-11-20 04:24:15 -0300

    jfdev

---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Bem vindo ao <a href="/">JFDev!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Site criado com o intúito de aprimorar minhas habilidades <br/>e boas práticas dos recursos e ferramentas mais atuais do mercado.
+        </p>
+
+        <div className={styles.grid}>
+          <a href="/portfolio" className={styles.card}>
+            <h3>Portfolio &rarr;</h3>
+            <p>Informações de trabalhos onde participei/criei direta e indiretamente.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/dev_actv.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
---
+++ pages/portfolio.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Portfolio() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <p>Portfolio</p>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/dev_actv.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
